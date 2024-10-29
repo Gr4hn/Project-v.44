7317ea6dff2ded9dca2384c288524413ef3946d0
Fredrik Storm <fredrik.storm@chasacademy.se>
2024-10-29 11:33:10 +0100

    move menu to function

--- hangman.cpp
+++ hangman.cpp
@@ -16,9 +16,8 @@ class Player
     //Kod för Player
 };
 
-void endGame(bool& gameIsRunning);
-int main ()
-{
+void showMenu(vector<string>& words) {
+    int choice;
     bool gameIsRunning = true;
     cout <<"Välkommen till spelet Hänga Gubbe!" << endl <<endl;
     do
@@ -29,7 +28,6 @@ int main ()
         << "3. Instruktioner" << endl
         << "4. Avsluta" << endl
         << "Ditt val: ";
-        int choice;
         cin >> choice;
         switch(choice)
         {
@@ -43,7 +41,8 @@ int main ()
                 cout <<"Instruktioner" << endl;
                 break;
             case 4:
-                endGame(gameIsRunning);
+                //endGame(gameIsRunning);
+                exit(0);
                 break;
             default:
                 cout <<"Felaktigt val, försök igen!" << endl;
@@ -51,10 +50,33 @@ int main ()
         }
     } while (gameIsRunning);
     cout <<"Tack för att du spelade!" << endl;
-    return 0;
 }
 
-void endGame(bool& gameIsRunning)
-{
+vector<string> loadWordsFromFile(const string& filename) {
+    vector<string> words;
+    ifstream file(filename);
+    if (!file) {
+        cerr << "Could not open the file " << filename << "\n";
+        return words;
+    }
+
+    string word;
+    while (getline(file, word)) {
+        if (!word.empty()) {
+            words.push_back(word);
+        }
+    }
+    return words;
+}
+
+void endGame(bool& gameIsRunning) {
     gameIsRunning = false;
+}
+
+int main ()
+{
+    vector<string>words = loadWordsFromFile("words.txt");
+    showMenu(words);
+    
+    return 0;
 }
