daa1ff5125ab55dd8f09fc9754a1018ead37b464
Fredrik Storm <fredrik.storm@chasacademy.se>
2024-10-29 22:04:05 +0100

    test drawing hang man

---
+++ hang.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+        cout << "\n    ----\n   |  \\|\n";
+        cout << "   " << "o" << "   |\n";
+        cout << "  " << "/" << "|" << "\\" << "  |\n";
+        cout << "  " << "/" << " " << "\\" << "  |\n";
+        cout << "       |\n  ------\n";
+    return 0;
+}
