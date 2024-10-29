1d360b1dd02fc64b002b7de63b3936df9c72835b
Eric Grahn <eric.grahn@chasacademy.se>
2024-10-29 08:51:00 +0100

    Test av första koden

--- main.cpp
+++ main.cpp
@@ -8,7 +8,21 @@ using namespace std;
 
 int main ()
 {
-    //kodningen
+    cout << "Välkommen till spelet Hänge gubbe!" << endl << endl;
+    do {
+        cout << "Välj ett alternativ" << endl;
+        cout << "1. Spela" << endl;
+        cout << "2. Highsciores" << endl;
+        cout << "3. Instruktioner" << endl;
+        cout << "4. Avsluta" << endl;
+        
+        cout << "Ditt val: ";
+        int choice = 0;
+        cin >> choice;
+        
 
+    } while (1);
+
+    cout << "Tack för att du spelade!" << endl;
     return 0;
 }
